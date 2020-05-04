4b4d2f18da507e2186011627d3a1a2bee2967638
living4574 <adolfd@naver.com>
2020-05-04 15:48:27 +0900

    heroes property chagned to object type from string using interface

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
+  //hero 프로퍼티 추가, Windstorm
+  hero: Hero={
+    id:1,
+    name:'WindStorm'
+  };
+  
   constructor() { }
 
   ngOnInit(): void {
   }
+  //ngOnInit은 라이프싸이클 후킹 함수 입니다. 
+  //Angular는 컴포넌트를 생성한 직후에 ngOnInit를 호출합니다. 
+  //그래서 컴포넌트를 초기화하는 로직은 이 메소드에 작성하는 것이 좋습니다.
+
 
 }
