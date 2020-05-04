a545adc8ba3d01925ed5f82a8eec2669c5783f01
living4574 <adolfd@naver.com>
2020-05-04 21:08:52 +0900

    person.ts

---
+++ typescriptPractice/person.js
@@ -0,0 +1,12 @@
+"use strict";
+var Person = /** @class */ (function () {
+    function Person(name) {
+        this.name = name;
+    }
+    Person.prototype.sayHello = function () {
+        return "Hello, " + this.name;
+    };
+    return Person;
+}());
+var person = new Person('Lee');
+console.log(person.sayHello());
