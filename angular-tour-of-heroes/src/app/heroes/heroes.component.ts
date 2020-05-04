89f99d15a7656e60230af9d4966be7fcce204c00
living4574 <adolfd@naver.com>
2020-05-04 22:04:51 +0900

    change synchronous to asynchronous by Observable, subscribe from rxjs

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -21,7 +21,7 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void{
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().subscribe(heroes=> this.heroes=heroes);
   }
 
   constructor(
@@ -34,6 +34,7 @@ export class HeroesComponent implements OnInit {
   ngOnInit(): void{
     this.getHeroes();
   }
+  
   //ngOnInit은 라이프싸이클 후킹 함수 입니다. 
   //Angular는 컴포넌트를 생성한 직후에 ngOnInit를 호출합니다. 
   //그래서 컴포넌트를 초기화하는 로직은 이 메소드에 작성하는 것이 좋습니다.
