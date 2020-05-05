47b1420db76e869e4c47748027b456ca45cc3a19
live4574 <adolfd@naver.com>
2020-05-05 17:14:23 +0900

    HttpClient InMemoryWEbAPiModule added

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { DashboardComponent } from './dashboard/dashboard.component'; //<--NgMod
 
 import { HttpClientModule } from '@angular/common/http';
 
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import {InMemoryDataService } from './in-memory-data.service';
 
 @NgModule({
   declarations: [
@@ -26,6 +28,14 @@ import { HttpClientModule } from '@angular/common/http';
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
+    //HttpClientInMemroyWebApiModule 모듈은 HTTP 요청을 가로채고
+    //서버의 응답을 흉내냅니다.
+    //실제 서버가 준비되면 이 부분을 제거하면 됩니다.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService,{dataEncapsulation:false}
+    )
+    //forRoot() 메소드는 InMemroyDataService 클래스를 인자로 받아서
+    //인-메모리 데이터베이스의 실행환경을 구성하는 메소드입니다.
   ],
   providers: [],
   bootstrap: [AppComponent]
