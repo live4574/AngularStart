8220580f172c8ff9033443b3ace65155723a0acc
live4574 <adolfd@naver.com>
2020-05-06 16:18:32 +0900

    data binding added

--- displaying-data/src/app/app.component.ts
+++ displaying-data/src/app/app.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-root',
   template: `
   <h1>{{title}}</h1>
-  <h2>My favorite hero is: {{myHero}}</h2>
+  <h2>My favorite hero is: {{myHero.name}}</h2>
   <p>Heroes:</p>
     <ul>
       <li *ngFor="let hero of heroes">
-      {{hero}}
+      {{hero.name}}
       </li>
     </ul>
+    <p *ngIf="heroes.length > 3">There are many heroes!</p>
   `
 })
 //역따옴표는 문자열을 여러줄에 걸쳐 선언하는 ECMAScript 2015 표준
@@ -18,12 +20,18 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'Tour of Heroes'
-  heroes = ['Windstorm' , 'Bombasto', 'Magneta', 'Tornado'];
+  heroes=[
+    new Hero(1, 'WindStorm'),
+    new Hero(13, 'Bombasto'),
+    new Hero(15, 'Magneta'),
+    new Hero(20, 'Tornado')
+  ];
   myHero = this.heroes[0];
 
   constructor(){
 
   }
+  
 }
 
 //키 입력이나 타이머, HTTP 응답과 같은 비동기 이벤트가
@@ -32,3 +40,4 @@ export class AppComponent {
 
 //셀렉터 항목에 <App-root>가 지정되어 있고, index.html
 //파일에는 <app-root>가 작성되어 있음.
+
