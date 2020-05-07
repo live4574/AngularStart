937b5dda73ab250323985eb4685ea65b7250c067
live4574 <adolfd@naver.com>
2020-05-07 11:37:27 +0900

    FormControl class load and FormControl instance code class property declaration at name-editor.component.ts

--- reactive-forms/src/app/name-editor/name-editor.component.ts
+++ reactive-forms/src/app/name-editor/name-editor.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+
+import {FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-name-editor',
   templateUrl: './name-editor.component.html',
   styleUrls: ['./name-editor.component.css']
 })
-export class NameEditorComponent {
-  name = new FormControl('');
+export class NameEditorComponent implements OnInit {
 
-  updateName() {
-    this.name.setValue('Nancy');
+  name= new FormControl('');
+  constructor() { }
+
+  ngOnInit() {
   }
+
 }
