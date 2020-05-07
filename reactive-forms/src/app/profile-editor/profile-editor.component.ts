9c5e2f53ed9cc858f3e61551b8bc23dc69ad0a9f
live4574 <adolfd@naver.com>
2020-05-07 12:39:19 +0900

    LastName and FirstName Submitbutton and console OnSubmit added

--- reactive-forms/src/app/profile-editor/profile-editor.component.ts
+++ reactive-forms/src/app/profile-editor/profile-editor.component.ts
@@ -10,12 +10,16 @@ export class ProfileEditorComponent implements OnInit {
 
   profileForm= new FormGroup({
     firstName: new FormControl(''),
-    lastname: new FormControl(''),
+    lastName: new FormControl(''),
   })
   //폼 컨트롤을 그룹으로 묶음.
   //폼 컨트롤의 값을 번거롭게 하나씩 참조할 필요 없이
   // 폼그룹에서 제공하는 모델 사용가능
-  
+
+  onSubmit(){
+    //TODO: EventEmitter로 폼 내용 보내기
+    console.warn(this.profileForm.value);
+  }
   constructor() { }
 
   ngOnInit() {
