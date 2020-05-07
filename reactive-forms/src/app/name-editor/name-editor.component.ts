60cec1c493f0d4eb88ab235a172d3b9290e6b2f5
live4574 <adolfd@naver.com>
2020-05-07 12:01:50 +0900

    nancy update name method added to name-editor.componenet.html

--- reactive-forms/src/app/name-editor/name-editor.component.ts
+++ reactive-forms/src/app/name-editor/name-editor.component.ts
@@ -12,10 +12,15 @@ export class NameEditorComponent implements OnInit {
   name= new FormControl('');
   //FormControl 생성자
   //인자로 빈 문자열 초기값 전달, 설정
-  
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  updateName(){
+    this.name.setValue('Nancy');
+  }
+
 
 }
