708805ea0699f241f9cf9384a409a0b30245016d
live4574 <adolfd@naver.com>
2020-05-07 12:14:19 +0900

    profileForm FirstName, Last Name added to profile-ediotor.component.html

--- reactive-forms/src/app/profile-editor/profile-editor.component.ts
+++ reactive-forms/src/app/profile-editor/profile-editor.component.ts
@@ -12,6 +12,10 @@ export class ProfileEditorComponent implements OnInit {
     firstName: new FormControl(''),
     lastname: new FormControl(''),
   })
+  //폼 컨트롤을 그룹으로 묶음.
+  //폼 컨트롤의 값을 번거롭게 하나씩 참조할 필요 없이
+  // 폼그룹에서 제공하는 모델 사용가능
+  
   constructor() { }
 
   ngOnInit() {
