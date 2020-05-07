704817d8eb83008e6a42cbb37c62a99536792194
live4574 <adolfd@naver.com>
2020-05-07 16:12:51 +0900

    load formsModule and add in @Ngmodule imports to use forms based in template at entire application

--- angular-forms/src/app/app.module.ts
+++ angular-forms/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeroFormComponent } from './hero-form/hero-form.component';
 
+import {FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent,
     HeroFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
