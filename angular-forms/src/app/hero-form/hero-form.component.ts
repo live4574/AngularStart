8d71e7b563d6433113bb9e9b7fef6436dfe9cb56
live4574 <adolfd@naver.com>
2020-05-07 16:06:39 +0900

    get Hero model, made dummy data model, powers

--- angular-forms/src/app/hero-form/hero-form.component.ts
+++ angular-forms/src/app/hero-form/hero-form.component.ts
@@ -9,9 +9,23 @@ import { Hero } from '../hero';
 })
 export class HeroFormComponent implements OnInit {
 
+  powers=['really smart','flexible'];
+
+  model= new Hero(18,'Dr IQ',this.powers[0],'Chuck overstreet');
+
+  submitted=false;
+
+  onSubmit(){this.submitted=true;}
+
+  //TODO: 필요한 기능을 개발하고 나면 이 함수는 제거.
+  get diagnostic(){
+    return JSON.stringify(this.model);
+  }
+  
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 }
