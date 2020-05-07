78b0b85fa597012d745dc38d44f7f320d3e6fe24
live4574 <adolfd@naver.com>
2020-05-07 12:50:03 +0900

    nested in group address added to profile-editor.component.ts

--- reactive-forms/src/app/profile-editor/profile-editor.component.ts
+++ reactive-forms/src/app/profile-editor/profile-editor.component.ts
@@ -11,7 +11,14 @@ export class ProfileEditorComponent implements OnInit {
   profileForm= new FormGroup({
     firstName: new FormControl(''),
     lastName: new FormControl(''),
-  })
+    address: new FormGroup({
+      street: new FormControl(''),
+      city: new FormControl(''),
+      state: new FormControl(''),
+      zip : new FormControl('')
+    })
+  });
+  
   //폼 컨트롤을 그룹으로 묶음.
   //폼 컨트롤의 값을 번거롭게 하나씩 참조할 필요 없이
   // 폼그룹에서 제공하는 모델 사용가능
