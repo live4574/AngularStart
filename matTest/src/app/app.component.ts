5a7e2f5b76e956fa951276ea4776015a8970ee4b
live4574 <adolfd@naver.com>
2020-05-08 17:55:00 +0900

    style added and matform modified

--- matTest/src/app/app.component.ts
+++ matTest/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'matTest';
+
+  myControl: FormControl;
+
+  constructor(private fb: FormBuilder) {
+
+    this.myControl = this.fb.control('')
+  }
 }
--- matTest/src/app/app.module.ts
+++ matTest/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSliderModule} from '@angular/material/slider'
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
 import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+
+import {MatInputModule} from '@angular/material/input'
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,8 +18,12 @@ import {MatRadioModule} from '@angular/material/radio';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
     MatSliderModule,
     MatRadioModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
